03d387fbaa244de3905787245535e89559c157bb
Farah-Tanveer <farahtanveer518@gmail.com>
2026-02-02 21:45:19 +0500

    Day-1

---
+++ Day_1/script.js
@@ -0,0 +1,7 @@
+console.log("Hello Javascript");
+console.log("Name:","Farah","\nAge:",18);
+console.log("University Name:","PUCIT");
+console.log(10+5);
+console.log(20-7);
+console.log(4*3);
+console.log("I am learning JavaScript");
