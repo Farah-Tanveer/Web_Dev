40df84cbd7aeb2177383eb68e4935301447eead9
Farah-Tanveer <farahtanveer518@gmail.com>
2026-02-04 20:43:41 +0500

    Day 2 Practice

---
+++ Day_2/practice.js
@@ -0,0 +1,36 @@
+//TASK 1
+let Name='Farah'
+let age=18
+let dept='PUCIT'
+let isStudent=true
+
+console.log("Name:",Name)
+console.log("Age:",age)
+console.log("Department:",dept)
+console.log("Are you a student?",isStudent)
+
+//TASK 2
+let marks1 = 85
+let marks2 = 90
+let marks3 = 88
+
+//Calculate
+let total_marks=marks1+marks2+marks3;
+let average_marks=total_marks/3;
+
+//print
+console.log("Tolal Marks:",total_marks)
+console.log("Average Marks:",average_marks)
+
+//TASK 3
+console.log(typeof Name)
+console.log(typeof age)
+console.log(typeof dept)
+console.log(typeof isStudent)
+
+//TASK 4
+let x = "5";
+let y = 2;
+console.log(x + y); // "52"
+//52 because x is a string and y is a number and adding them results in
+//  concetenation number also changes to string
---
+++ Day_2/script.js
@@ -0,0 +1,34 @@
+//numbers
+let age=18
+console.log(age+10);
+console.log(age+4);
+
+//string
+let Name="Farah";
+let uni='PUCIT';
+console.log(Name);
+console.log(uni);
+
+//constant
+const pi = 3.1416 //can't change value
+console.log(pi);
+
+//Boolean
+let isStudent=true;
+let isPassed=false;
+console.log(isStudent);
+console.log(isPassed);
+
+//null values
+let x=null;
+console.log(x);
+
+//typeof operator
+console.log(typeof 18);
+console.log(typeof Name);
+console.log(typeof isStudent);
+
+//string + number concetenation
+let a=10;
+let b='10';
+console.log(a+b);
