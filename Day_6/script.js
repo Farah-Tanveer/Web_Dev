bb9913dc8a64e527cae4303263468fbd41cc13fc
Farah-Tanveer <farahtanveer518@gmail.com>
2026-02-05 18:40:17 +0500

    Day 6 Practice

---
+++ Day_6/script.js
@@ -0,0 +1,34 @@
+//TASK 1
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+//TASK 2
+let n=10;
+while(n>0){
+    console.log(n);
+    n--;
+}
+
+//TASK 3
+for(let i=1;i<=20;i++){
+    if(i%2===0)
+        console.log(i);
+}
+
+//TASK 4
+let sum=0;
+for(let i=1;i<=10;i++){
+    sum+=i;
+}
+console.log(sum);
+
+//Task 5
+for(let i=1;i<=5;i++){
+    let row="";
+    for(let j=1;j<=i;j++){
+        row+="*"
+    }
+    console.log(row);
+}
+
