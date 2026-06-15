02c32473572b46fd2e281d7f2a4c1dd27c16964e
Farah-Tanveer <farahtanveer518@gmail.com>
2026-06-15 20:04:40 +0500

    created a simple counter app using states

---
+++ REACT/counter_project/src/App.jsx
@@ -0,0 +1,32 @@
+import {useState} from 'react'
+
+function App() {
+
+  let [counter,setCounter] = useState(15) //returns an array
+  //let count = 15
+
+  const addValue = ()=>{
+    //counter= counter+1
+    setCounter(counter+1)
+    console.log("value added",counter);
+  }  
+
+  const removeValue = ()=>{
+    setCounter(counter-1)
+  } 
+return (
+    <>
+    <h1>React with vite</h1>
+    <h2>Counter value: {counter}</h2>
+
+    <button onClick={addValue}>Add value</button>
+    <br />
+    <button
+    onClick={removeValue}>Remove value</button>
+
+    <p>footer: {counter}</p>
+    </>
+  )
+}
+
+export default App
